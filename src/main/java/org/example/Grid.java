29fa942053fca0a4e5da4d678b7b16763ce94565
sahil khadayate <sahil.khadayate_ftc@external.swiggy.in>
2025-02-02 14:35:52 +0530

    feat: Add positive rows and negative columns logic to grid constructor

--- src/main/java/org/example/Grid.java
+++ src/main/java/org/example/Grid.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class Grid {
     private final int rows;
     private final int columns;
-    private final List<List<Cell>> grid;
 
     public Grid(int rows, int columns){
-
-        throw new IllegalArgumentException();
+        if(rows <= -1 || columns == -1){
+            throw new IllegalArgumentException();
+        }
+        this.rows = rows;
+        this.columns = columns;
     }
 }
--- src/test/java/org/example/GridTest.java
+++ src/test/java/org/example/GridTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 
 public class GridTest {
@@ -9,5 +10,25 @@ public class GridTest {
     @Test
     public void testGridCreationThrowsExceptionWhenRowIsNegative1(){
         assertThrows(IllegalArgumentException.class, () -> new Grid(-1, 1));
+    }
+
+    @Test
+    public void testGridCreationThrowsExceptionWhenColumnIsNegative1(){
+        assertThrows(IllegalArgumentException.class, () -> new Grid(1, -1));
+    }
+
+    @Test
+    public void testGridCreationThrowsExceptionWhenColumnAndRowIsNegative1(){
+        assertThrows(IllegalArgumentException.class, () -> new Grid(-1, -1));
+    }
+    @Test
+    public void testGridCreationThrowsExceptionWhenColumnIs10AndRowIsNegative10(){
+        assertThrows(IllegalArgumentException.class, () -> new Grid(10, -1));
+    }
+
+    @Test
+    public void testGridCreatedWhenRowIs1AndColumnIs1(){
+        Grid grid = new Grid(1, 1);
+        assertNotNull(grid);
     }
 }
